18fc621127f9ff336dde3b0c437af0ed33a1bb0c
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-10 17:26:41 +0900

    update index.php

--- index.php
+++ index.php
@@ -1,20 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP MySQL Todo App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body>
-    <header>
-        <p class="fs-3">PHP MySQL Todo App</p>
-    </header>
-    <main>
-        <div class="container">
 
+<body>
+    <?php include('header.php'); ?>
+    <main>
+        <div class="container mt-5">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>URL</th>
+                    <th>DEADLINE</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>HTML</td>
+                        <td>http://localhost/php_crud/</td>
+                        <td>2024-05-25</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>HTML</td>
+                        <td>http://localhost/php_crud/</td>
+                        <td>2024-05-25</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>HTML</td>
+                        <td>http://localhost/php_crud/</td>
+                        <td>2024-05-25</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
+
 </html>
