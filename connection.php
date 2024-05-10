6e4a49f8d20573ba028e7a69dc2b55f9ca798394
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-10 17:44:38 +0900

    delete echo

--- connection.php
+++ connection.php
@@ -7,6 +7,4 @@ $conn = new mysqli($servername, $username, $password, $db_name, 3306);
 if($conn->connect_error) {
     die("Connection failed".$conn->connect_error);
 }
-echo "Successfull connection!";
-
 ?>
