607a138b600e6b0694e43a5abbaa17ff1b3dca00
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-10 17:48:10 +0900

    update link

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header class="p-2 shadow">
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">PHP MySQL Todo App</a>
+            <a class="navbar-brand" href="./">PHP MySQL Todo App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
