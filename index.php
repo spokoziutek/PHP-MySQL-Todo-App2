9d27bf5cbc51b0337f578f33b1588786a2904d10
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-10 19:43:35 +0900

    add button

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-light">
     <?php include('header.php'); ?>
     <main>
         <div class="container mt-5">
@@ -19,6 +19,7 @@
                         <th>NAME</th>
                         <th>URL</th>
                         <th>DEADLINE</th>
+                        <th>ACTIONS</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,13 +30,17 @@
                     if (!$result) {
                         die("Invalid query!");
                     }
-                    while ($row=$result->fetch_assoc()) {
+                    while ($row = $result->fetch_assoc()) {
                         echo "
                     <tr>
                         <td>$row[id]</td>
                         <td>$row[name]</td>
                         <td>$row[url]</td>
                         <td>$row[deadline]</td>
+                        <td>
+                        <a class='btn btn-success' href='edit.php?id=$row[id]'>Edit</a>
+                        <a class='btn btn-danger' href='delete.php?id=$row[id]'>Delete</a>
+                        </td>
                     </tr>
                     ";
                     }
