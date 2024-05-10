b773f20d979e52b3f097b65a0c4891afd77c8d72
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-10 17:26:31 +0900

    add header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+<header class="p-2 shadow">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">PHP MySQL Todo App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./addtask">Add Task</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
