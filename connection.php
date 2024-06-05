ce8b3da3c5781637b975c031949791772e0e9e9a
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-05 10:54:41 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "phpmysqltodo-server.mysql.database.azure.com";
 $username = "titsnlcplv";
 $password = "qweasdzxc123!";
-$db_name = "phpcrud";
+$db_name = "phpmysqltodo-database";
 $conn = new mysqli($servername, $username, $password, $db_name, 3306);
 if($conn->connect_error) {
     die("Connection failed".$conn->connect_error);
