67ebf67e31bf47a43a4bf995e3dcdeac33e80d45
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-05 10:40:24 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
+$servername = "phpmysqltodo-server.mysql.database.azure.com";
+$username = "titsnlcplv";
+$password = "qweasdzxc123!";
 $db_name = "phpcrud";
 $conn = new mysqli($servername, $username, $password, $db_name, 3306);
 if($conn->connect_error) {
